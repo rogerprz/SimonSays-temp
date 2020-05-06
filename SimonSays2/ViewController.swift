a7da59338f21b89bc8779c9c8d7e98a688253d16
rogerprz <rogerprz@github.com>
2020-05-06 00:50:04 -0700

    Initial Commit

---
+++ SimonSays2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimonSays2
+//
+//  Created by Roger Perez on 5/6/20.
+//  Copyright © 2020 Roger Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
