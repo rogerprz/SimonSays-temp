3cbb83c2d84e2a18a31a008c5820a4b2a2eea686
rogerprz <rogerprz@github.com>
2020-05-07 23:12:50 -0700

    Simon Says 4 completed

--- SimonSays2/ViewController.swift
+++ SimonSays2/ViewController.swift
@@ -76,7 +76,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func colorButtonHandler(_ sender: CircularButton) {
-        print("Button \(sender.tag) tapped")
+        if sender.tag == colorsToTap.removeFirst(){
+            
+        } else {
+            for button in colorButtons {
+                button.isEnabled = false
+            }
+            return
+        }
+        if colorsToTap.isEmpty {
+            for button in colorButtons {
+                button.isEnabled = false
+            }
+            actionButton.setTitle("Continue", for: .normal)
+            actionButton.isEnabled = true
+        }
     }
     
     @IBAction func actionButtonHandler(_ sender: UIButton) {
